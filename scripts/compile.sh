71959f88aed7f48871612ad78b51d967eea65583
miun173 <fahmifan173@gmail.com>
2019-02-26 13:20:28 +0700

    feat: add window build script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-export GOBIN=$PWD/output 
-echo $GOBIN
-go install ./cmd/...
+
+echo "start compile to:"
+GOBIN=$PWD/output go install ./cmd/...;
+GOBIN=$PWD/output/ GOOS=windows GOARCH=amd64 go build ./cmd/...;
+mv $PWD/*.exe $PWD/output;
+echo $PWD/output; ls $PWD/output;
+echo "finsihed compile";
